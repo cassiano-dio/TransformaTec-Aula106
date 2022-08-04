d6164e5d43b66065b028b6bc8eff060264f98fc3
Cassiano Ricardo de Oliveira Peres <97028976+cassiano-dio@users.noreply.github.com>
2022-08-04 15:52:30 -0300

    Update ProductServiceController.java

--- src/main/java/com/api/example/controllers/ProductServiceController.java
+++ src/main/java/com/api/example/controllers/ProductServiceController.java
@@ -12,27 +12,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.example.models.Cart;
 import com.api.example.models.Product;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 /**
  * Endpoints da API REST para manipular a classe Product
  * 
- * 1 - POST: criar um novo produto
- * 2 - GET: buscar um produto
- * 3 - GET: listar produtos
- * 4 - PUT: atualizar um produto
- * 5 - DELETE: remover um produto
+ *  1 - POST: criar um novo produto
+ *  2 - GET: buscar um produto
+ *  3 - GET: listar produtos
+ *  4 - PUT: atualizar um produto
+ *  5 - DELETE: remover um produto
  */
 
 @RestController
-@Api(value = "API para manutenção de produtos")
-@CrossOrigin(value = "*")
+@CrossOrigin(origins = "*")
 public class ProductServiceController {
-
+    
     private static List<Product> products = new ArrayList<Product>();
+
+    private static Cart cart;
 
     static {
 
@@ -44,37 +45,47 @@ public class ProductServiceController {
 
     }
 
-    //Adicionar um novo produto
+
+    //Pesquisando um produto por Id
+    @ApiOperation(value = "Buscando um produto por Id")
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Object> getProduct(@PathVariable("id") int id){
+
+        return new ResponseEntity<>(products.get(id), HttpStatus.OK);
+
+    }
+
     @ApiOperation(value = "Adicionar um novo produto")
-    @RequestMapping(value = "/new/products", method = RequestMethod.POST )
+    @RequestMapping(value = "/products", method = RequestMethod.POST, produces="application/json", consumes="application/json")
     public ResponseEntity<Object> createProduct(@RequestBody Product product){
 
+        System.out.println("-------->> Product:");
+        System.out.println(product);
+
         products.add(product);
-        return new ResponseEntity<>("Produto cadastrado com sucesso!", HttpStatus.CREATED);
+        return new ResponseEntity<>("Produto inserido com sucesso!", HttpStatus.OK);
 
     }
 
-    //Buscar um produto por id
-    @ApiOperation(value = "Buscar um produto")
-    @RequestMapping(value = "/get/products/{id}")
-    public ResponseEntity<Object> getProduct(@PathVariable("id") int id){
-        
-        return new ResponseEntity<>(products.get(id), HttpStatus.OK);
-    }
+    //Pesquisando um produto por Id
+    // @RequestMapping(value = "/products/{id}")
+    // public ResponseEntity<Object> getProduct(@PathVariable("id") int id){
 
-    //Listar todos os produtos
-    @ApiOperation(value = "Listar todos os produtos")
-    @RequestMapping(value = "/all/products", method = RequestMethod.GET)
+    //     return new ResponseEntity<>(products.get(id), HttpStatus.OK);
+
+    // }
+
+    //Listando todos os produtos
+    @ApiOperation(value="Listar todos os produtos")
+    @RequestMapping(value = "/products",method = RequestMethod.GET, produces="application/json")
     public ResponseEntity<Object> getProducts(){
-
-        return new ResponseEntity<>(products, HttpStatus.OK);
-
+        return new ResponseEntity<>(products,HttpStatus.OK);
     }
 
-    // Atualizando um produto por Id
-    @ApiOperation(value = "Atualizar um produto por Id")
-    @RequestMapping(value = "/update/products/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Object> updateProduct(@PathVariable("id") int id, @RequestBody Product product) {
+    //Atualizando um produto por Id
+    @ApiOperation(value = "Atualizar informações de um produto")
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT, produces="application/json", consumes="application/json")
+    public ResponseEntity<Object> updateProduct(@PathVariable("id") int id, @RequestBody Product product){
 
         products.remove(id);
         product.setId(id);
@@ -84,14 +95,21 @@ public class ProductServiceController {
 
     }
 
-    // Removendo um produto por Id
-    @ApiOperation(value = "Remover um produto por Id")
-    @RequestMapping(value = "/remove/products/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Object> deleteProduct(@PathVariable("id") int id) {
+    //Removendo um produto por Id
+    @ApiOperation(value = "Excluir um produto")
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE, produces="application/json")
+    public ResponseEntity<Object> deleteProduct(@PathVariable("id") int id){
 
         products.remove(id);
         return new ResponseEntity<>("Produto removido com sucesso!", HttpStatus.OK);
 
+    }
+
+    @ApiOperation(value = "Criar novo carrinho")
+    @RequestMapping(value = "/cart", method = RequestMethod.POST, produces="application/json")
+    public ResponseEntity<Object> createCart(@RequestBody int id){
+        cart = new Cart(id);
+        return new ResponseEntity<>("Carrinho criado com sucesso!", HttpStatus.OK);
     }
 
 }
