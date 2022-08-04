881370dc2cd27cb59ead0f795dd4bf00b622cbc9
Cassiano Ricardo de Oliveira Peres <97028976+cassiano-dio@users.noreply.github.com>
2022-08-04 15:53:23 -0300

    Update SwaggerConfig.java

--- src/main/java/com/api/example/config/SwaggerConfig.java
+++ src/main/java/com/api/example/config/SwaggerConfig.java
@@ -3,10 +3,10 @@ package com.api.example.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.Contact;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -15,22 +15,23 @@ public class SwaggerConfig {
     
     @Bean
     public Docket api(){
-
+        
         return new Docket(DocumentationType.SWAGGER_2)
                     .select()
                     .apis(RequestHandlerSelectors.basePackage("com.api.example"))
                     .build()
-                    .apiInfo(info());
+                    .useDefaultResponseMessages(false)
+                    .apiInfo(apiInfo());
 
     }
 
-    private ApiInfo info() {
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                    .title("API de produtos de PetShop")
-                    .description("API para manutenção de registros de produtos de PetShop")
+                    .title("API de Produtos")
+                    .description("Um exemplo de API documentada.")
                     .version("1.0.0")
                     .license("Apache License Version 2.0")
-                    .contact(new Contact("Cassiano", "cassiano.dev", "cassiano@dev.com"))
+                    .contact(new Contact("Cassiano", "cassiano-dev.com", "cassiano@dev.com"))
                     .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
                     .build();
     }
